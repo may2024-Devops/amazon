c1802fc1d845053c5ba7d1797400b48825252fb1
ganeshcoderepo <ganeshramakrishna59@gmail.com>
2024-06-17 19:28:11 +0530

    Update DBUtils.java

--- DBUtils.java
+++ DBUtils.java
@@ -4,3 +4,4 @@ Hi adding new line into this file on 28th may
 
 Hi adding this line in development branch
 updating this line to see the difference for git pull and git fetch
+  updating this line to see the difference for git pull only
