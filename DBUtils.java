88b71bd458c5705e5e57290a6beba599b69555db
ganeshcoderepo <ganeshramakrishna59@gmail.com>
2024-06-17 10:56:11 +0530

    Update DBUtils.java

--- DBUtils.java
+++ DBUtils.java
@@ -3,3 +3,4 @@ Hello..This DBUtils files contains db connection logic
 Hi adding new line into this file on 28th may
 
 Hi adding this line in development branch
+updating this line to see the difference for git pull and git fetch
